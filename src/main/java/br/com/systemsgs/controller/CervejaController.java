62d7c107d0bcee8bee31554cc255b3df6b56f6c3
Guilherme Santos <guiromanno@gmail.com>
2021-07-23 12:57:57 -0300

    Adicionando várias configurações

--- src/main/java/br/com/systemsgs/controller/CervejaController.java
+++ src/main/java/br/com/systemsgs/controller/CervejaController.java
@@ -1,13 +1,29 @@
 package br.com.systemsgs.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import br.com.systemsgs.model.ModelCerveja;
 
 @Controller
 public class CervejaController {
 	
-	@RequestMapping("/cervejas/novo")
+	@RequestMapping(value = "/cervejas/novo")
 	public String novo() {
+		return "cerveja/testando";
+	}
+	
+	@RequestMapping(value = "/cervejas/novo", method = RequestMethod.POST)
+	public String cadastrar(@Valid ModelCerveja modelCerveja, BindingResult result) {
+		
+		if(result.hasErrors()) {
+			System.out.println("tem sim");
+		}
+		
 		return "cerveja/testando";
 	}
 
---
+++ src/main/java/br/com/systemsgs/model/ModelCerveja.java
@@ -0,0 +1,35 @@
+package br.com.systemsgs.model;
+
+import org.hibernate.validator.constraints.NotBlank;
+
+public class ModelCerveja {
+	
+	@NotBlank(message = "SKU Obrigatório")
+	private String sku;
+	
+	private String nome;
+	
+	private String descricao;
+	
+	public String getSku() {
+		return sku;
+	}
+	public void setSku(String sku) {
+		this.sku = sku;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getDescricao() {
+		return descricao;
+	}
+	
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+}
