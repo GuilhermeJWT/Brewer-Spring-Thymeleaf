2905ffedb1f5794b3e0adc6de821e5a6a7cf1615
GuilhermeJWT <guiromanno@gmail.com>
2021-08-14 13:11:52 -0300

    Iniciando o Cadastro e inicio da Exibição da Foto na Tela

--- src/main/java/br/com/systemsgs/config/SalvaFotoLocal.java
+++ src/main/java/br/com/systemsgs/config/SalvaFotoLocal.java
@@ -2,9 +2,11 @@ package br.com.systemsgs.config;
 
 import static java.nio.file.FileSystems.getDefault;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.UUID;
 
 import org.springframework.web.multipart.MultipartFile;
 
@@ -20,6 +22,31 @@ public class SalvaFotoLocal implements FotoStorage{
 		criarPastas();
 	}
 	
+	@Override
+	public String salvarTemporariamente(MultipartFile[] files) {
+		String novoNome = null;
+		if(files != null && files.length>0) {
+			MultipartFile arquivo = files[0];
+			 novoNome = renomearArquivo(arquivo.getOriginalFilename());
+			try {
+				arquivo.transferTo(new File(this.localTemporario.toAbsolutePath().toString() + getDefault().getSeparator() + novoNome));
+			} catch (IllegalStateException | IOException e) {
+				throw new RuntimeException("Erro ao Salvar a Foto", e);
+			}
+			return novoNome;
+		}
+		return novoNome;
+	}
+	
+	@Override
+	public byte[] recuperarFotoTemporaria(String nome) {
+		try {
+			return Files.readAllBytes(this.localTemporario.resolve(nome));
+		} catch (IOException e) {
+			throw new RuntimeException("Erro ao ler a Foto para Exibir");
+		}
+	}
+	
 	public SalvaFotoLocal(Path path) {
 		this.local = path;
 		criarPastas();
@@ -28,16 +55,16 @@ public class SalvaFotoLocal implements FotoStorage{
 	private void criarPastas() {
 		try {
 			Files.createDirectories(this.local);
-			this.localTemporario = getDefault().getPath(this.local.toString(), "tem");
+			this.localTemporario = getDefault().getPath(this.local.toString(), "temp");
 			Files.createDirectories(this.localTemporario);
 		} catch (IOException e) {
 			throw new RuntimeException("Erro ao criar a Pasta para Salvar a Foto!");
 		}
 	}
 
-	@Override
-	public void salvarTemporariamente(MultipartFile[] files) {
-		System.out.println("Salvando temporariamente");
+	private String renomearArquivo(String nomeOriginal) {
+		String novoNome = UUID.randomUUID().toString() + "_" + nomeOriginal;
+		
+		return novoNome;
 	}
-	
 }
