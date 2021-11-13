83c23fccc5d884c032d6ce319c548e6060f513db
GuilhermeJWT <guiromanno@gmail.com>
2021-11-13 12:23:07 -0300

    Combos de Estados e Cidades Dinâmicos - Parte 2 - Adiciona Gif de carregamento

--- src/main/resources/static/javascripts/cliente.combo-estado-cidade.js
+++ src/main/resources/static/javascripts/cliente.combo-estado-cidade.js
@@ -1,18 +1,18 @@
 var Brewer = Brewer || {};
 
-Brewer.ComboEstado = (function(){
+Brewer.ComboEstado = (function() {
 	
-	function ComboEstado(){
+	function ComboEstado() {
 		this.combo = $('#estado');
 		this.emitter = $({});
 		this.on = this.emitter.on.bind(this.emitter);
 	}
 	
-	ComboEstado.prototype.iniciar = function(){
+	ComboEstado.prototype.iniciar = function() {
 		this.combo.on('change', onEstadoAlterado.bind(this));
 	}
 	
-	function onEstadoAlterado(){
+	function onEstadoAlterado() {
 		this.emitter.trigger('alterado', this.combo.val());
 	}
 	
@@ -20,24 +20,58 @@ Brewer.ComboEstado = (function(){
 	
 }());
 
-Brewer.ComboCidade = (function(){
+Brewer.ComboCidade = (function() {
 	
-	function ComboCidade(comboEstado){
+	function ComboCidade(comboEstado) {
 		this.comboEstado = comboEstado;
 		this.combo = $('#cidade');
+		this.imgLoading = $('.js-img-loading');
 	}
 	
-	ComboCidade.prototype.iniciar = function(){
-		this.comboEstado.on('alterado', onEstadoAlterado,bind(this));
+	ComboCidade.prototype.iniciar = function() {
+		reset.call(this);
+		this.comboEstado.on('alterado', onEstadoAlterado.bind(this));
 	}
 	
-	function onEstadoAlterado(evento, codigoEstado){
-		var resposta =$.ajax({
-			url: this.combo.data('url'),
-			method: 'GET',
-			contentType: 'application/json',
-			data: {'estado': codigoEstadp}
+	function onEstadoAlterado(evento, codigoEstado) {
+		if (codigoEstado) {
+			var resposta = $.ajax({
+				url: this.combo.data('url'),
+				method: 'GET',
+				contentType: 'application/json',
+				data: { 'estado': codigoEstado }, 
+				beforeSend: iniciarRequisicao.bind(this),
+				complete: finalizarRequisicao.bind(this)
+			});
+			resposta.done(onBuscarCidadesFinalizado.bind(this));
+		} else {
+			reset.call(this);
+		}
+	}
+	
+	function onBuscarCidadesFinalizado(cidades) {
+		var options = [];
+		cidades.forEach(function(cidade) {
+			options.push('<option value"' + cidade.codigo + '">' + cidade.nome + '</option>');
 		});
+		
+		this.combo.html(options.join(''));
+		this.combo.removeAttr('disabled');
+	}
+	
+	function reset() {
+		this.combo.html('<option value="">Selecione a cidade</option>');
+		this.combo.val('');
+		this.combo.attr('disabled', 'disabled');
+	}
+	
+	function iniciarRequisicao() {
+		reset.call(this);
+		this.imgLoading.show();
+	}
+	
+	function finalizarRequisicao() {
+		this.imgLoading.hide();
 	}
 	
 	return ComboCidade;
@@ -49,7 +83,7 @@ $(function() {
 	var comboEstado = new Brewer.ComboEstado();
 	comboEstado.iniciar();
 	
-	var comboCidade = Brewer.ComboCidade(comboEstado);
+	var comboCidade = new Brewer.ComboCidade(comboEstado);
 	comboCidade.iniciar();
 	
 });
