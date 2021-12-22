3110cd1a2b0376c46fc3e26570191996604ae4f3
GuilhermeJWT <guiromanno@gmail.com>
2021-12-21 21:12:02 -0300

    Melhroando nossa tela de Login e Autenticando em Memória

--- src/main/java/br/com/systemsgs/controller/LoginController.java
+++ src/main/java/br/com/systemsgs/controller/LoginController.java
@@ -1,5 +1,7 @@
 package br.com.systemsgs.controller;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,7 +11,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class LoginController {
 	
 	@GetMapping
-	public String login() {
+	public String login(@AuthenticationPrincipal User user) {
+		
+		if(user != null) {
+			return "redirect:/cervejas";
+		}
+		
 		return "Login";
 	}
 
